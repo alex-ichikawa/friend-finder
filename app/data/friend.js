e7cc245b52434562886eb02cb92fcfbbdc63ed91
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-10-24 21:53:57 -0700

    best match is working. jury rig popup best match page working

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,36 +1,68 @@
-let freinds = [
+let friends = [
     {
-        name: "test",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Kimi Raikkonen",
+        photo: "https://resize3.indiatvnews.com/en/resize/newbucket/715_-/2018/10/kimi-raikkonen-getty-1540190218.jpg",
         scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     },
     {
-        name: "test2",
-        photo: "someurl.com",
+        name: "Fernando Alonso",
+        photo: "https://e0.365dm.com/15/11/768x432/mclaren-sunbathing-alonso-seat-sunbath-fernando_3376727.jpg?20151114162754",
         scores: [
-            1,
+            3,
+            5,
             2,
             3,
-            4,
             5,
-            1,
+            2,
             2,
             3,
             4,
             5
         ]
+    },
+    {
+        name: "Sebastian Vettel",
+        photo: "https://www.gannett-cdn.com/-mm-/3d231402eae798b3509a0abb2192ec5dfd6181e9/c=327-0-3543-2418/local/-/media/2018/04/07/USATODAY/USATODAY/636587090600756502-vettel.jpg?width=534&height=401&fit=crop",
+        scores: [
+            5,
+            2,
+            4,
+            5,
+            5,
+            2,
+            2,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Max Verstappen",
+        photo: "https://msmproduction.s3-eu-west-1.amazonaws.com/s3fs-public/content/F1/2017/3%20%E2%80%93%20Bahrain/verstappenbahrain2017.jpg",
+        scores: [
+            5,
+            1,
+            3,
+            2,
+            5,
+            5,
+            1,
+            5,
+            4,
+            4
+        ]
     }
 ]
 
-module.exports = freinds;
+module.exports = friends;
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("app/public"));
 
+let scoreArray = [];
+let bestIndex;
+
 
 // Routes
 // =============================================================
@@ -22,26 +25,41 @@ app.get("/survey", function (req, res) {
 });
 
 app.get("/match", function (req, res) {
-    res.send("hi there");
+    // res.json(friends[bestIndex]);
+    res.send(`<html><body><h1>You're best match is</h1><br><h3>${friends[bestIndex].name}</h3><br><img src="${friends[bestIndex].photo}"</body></html>`)
 });
 
-// Displays all characters
+// Displays all friends
 app.get("/api/friends", function (req, res) {
     return res.json(friends);
 });
 
 // Create New Friends
-app.post("/api/friends", function(req, res) {
+app.post("/api/friends", function (req, res) {
     let newFriend = req.body
+    let o = 0
     friends.push(newFriend);
     res.json(newFriend);
+    // Find the best match
+    friends.forEach(function () {
+        let score = 0;
+        for (let i = 0; i < newFriend.scores.length; i++) {
+            if (parseInt(newFriend.scores[i]) - parseInt(friends[o].scores[i]) < 0) {
+                score += (parseInt(newFriend.scores[i]) - parseInt(friends[o].scores[i])) * -1;
+            }
+            else {
+                score += parseInt(newFriend.scores[i]) - parseInt(friends[o].scores[i]);
+            }
+
+        }
+        o++;
+        scoreArray.push(score);
+    });
+    // Remove current submission from array
+    scoreArray.pop();
+    bestIndex = scoreArray.indexOf(Math.min.apply(null, scoreArray));
+
 });
-
-
-
-
-
-
 
 
 app.listen(PORT, function () {
